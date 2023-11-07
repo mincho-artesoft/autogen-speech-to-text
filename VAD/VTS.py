2761f16ff7eea697714bcbf9756d7ed4418c2599
Aleksandar Iliev Svinarov <47814169+JatosDetking@users.noreply.github.com>
2023-11-07 11:01:28 +0200

    ....

--- VAD/VTS.py
+++ VAD/VTS.py
@@ -18,15 +18,19 @@ import certifi
 import os
 import threading
 import onnxruntime
+from flask_socketio import SocketIO
 
 SAMPLING_RATE = 16000
+
+audio_data = None
+samplerate = None
 
 torch.set_num_threads(1)
 os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
 
 ####################
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -46,10 +50,6 @@ pipe.model.config.forced_decoder_ids = whisper_processor.get_decoder_prompt_ids(
     language="english", task="translate")
 """  """
 
-app = Flask(__name__)
-sock = Sock(app)
-
-
 def process_speech(wav):
     window_size_samples = 1536
 
@@ -57,34 +57,30 @@ def process_speech(wav):
 @app.route('/')
 def index():
     return render_template('index5.html')
-
-@sock.route('/echo')
-def echo(sock):
-    while True:
-        data = sock.receive()
-        try:   
-            audio_data, samplerate = sf.read(io.BytesIO(data))
-            try:    
-                data, old_samplerate = sf.read("audio.wav")
-                combined_data = np.concatenate((data, audio_data), axis=0)
-                sf.write("audio.wav", combined_data, samplerate)
-            except:
-                sf.write("audio.wav", audio_data, samplerate)
-        except:
-             dataT = json.loads(data) 
-             if dataT["type"] == "stop":
-                 pipe = pipeline(
-                    "automatic-speech-recognition",
-                     model="openai/whisper-large-v2",
-                     chunk_length_s=30,
-                     device=device,
-)
-                 data, old_samplerate = sf.read("audio.wav")
-                 os.remove("audio.wav")
-                 prediction = pipe(data.copy(), batch_size=8)["text"]
-                 print(prediction)
-                 sock.send(prediction)
         
 
+@socketio.on('message')
+def handle_message(message):
+    global audio_data
+    global samplerate
+    
+    if message["type"] == "stop":
+        print("stop")
+        prediction = pipe(audio_data.copy(), batch_size=8)["text"]
+        print(prediction)
+        socketio.emit('message', prediction)
+    elif message["type"] == "start":
+        print("start")
+        sf.write("audio.wav", audio_data, samplerate)
+        audio_data = None
+    
+    if audio_data is None:
+        audio_data, samplerate = sf.read(io.BytesIO(message["file"]))
+    else:  
+        data, new = sf.read(io.BytesIO(message["file"]))
+        audio_data = np.concatenate((audio_data, data), axis=0)
+
+
+               
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    socketio.run(app, debug=True)
